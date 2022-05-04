f277def2211631cc2186111fe2417f41b334b08c
Amos Goh <amos.goh@websparks.sg>
2022-05-04 15:03:30 +0800

    Server Side.

--- server/routes/UserInput.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUserInputs, createUserInput } from "../controllers/UserInput.js";
-import UserInput from "../models/UserInput.js";
-const router = express.Router();
-
-router.get('/', getUserInputs);
-router.post('/', createUserInput);
-
-export default router;
